0e57b2edb2774a816cdf6b8e4abc226cf6a0eee2
Syouchan <syou5963@outlook.jp>
2024-10-16 11:34:59 +0900

    マテリアルの適用＋ログインフォームコンポーネントの生成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginformComponent } from "./component/loginform/loginform.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
